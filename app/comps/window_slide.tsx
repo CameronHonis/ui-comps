c3da13e6274615d7c0ad82c9a893638f064dd0fb
cameron <cameron-04@hotmail.com>
2024-09-28 08:58:32 -0500

    remove window slide interval

--- app/comps/window_slide.tsx
+++ app/comps/window_slide.tsx
@@ -3,45 +3,33 @@
 import clsx from "clsx";
 import React from "react";
 
-type Props = {
-    intervalMs: number;
-    className?: string;
-    style?: React.CSSProperties;
-    children: React.ReactNode[];
-}
+export default function WindowSlide(props: {
+	eleIdx: number;
+	className?: string,
+	style?: React.CSSProperties,
+	children: React.ReactNode[],
+}) {
+	let { current: sliderRef } = React.useRef<HTMLDivElement | null>(null);
 
-export default function WindowSlide(props: Props) {
-    const [pageIdx, setPageIdx] = React.useState(0);
-    let { current: sliderRef } = React.useRef<HTMLDivElement | null>(null);
 
-    React.useEffect(() => {
-        let _pageIdx = 0;
-        const intervalId = setInterval(() => {
-            _pageIdx = (_pageIdx + 1) % props.children.length;
-            setPageIdx(_pageIdx);
-        }, props.intervalMs);
+	React.useEffect(() => {
+		if (!sliderRef) return;
 
-        return () => clearInterval(intervalId);
-    }, [props.children.length, props.intervalMs]);
+		let accWidthPx = 0;
+		for (let i = 0; i < props.eleIdx; i++) {
+			const child = sliderRef.children[i];
+			const { width } = child.getBoundingClientRect();
+			accWidthPx += width;
+		}
 
-    React.useEffect(() => {
-        if (!sliderRef) return;
+		sliderRef.style.left = `-${accWidthPx}px`;
+	}, [sliderRef, props.eleIdx]);
 
-        let accWidthPx = 0;
-        for (let i = 0; i < pageIdx; i++) {
-            const child = sliderRef.children[i];
-            const { width } = child.getBoundingClientRect();
-            accWidthPx += width;
-        }
-
-        sliderRef.style.left = `-${accWidthPx}px`;
-    }, [sliderRef, pageIdx]);
-
-    return <div className={clsx("relative overflow-hidden", props.className)} style={props.style}>
-        <div className="absolute flex overflow-hidden transition-all duration-500" ref={el => { sliderRef = el }}>
-            {props.children.map((item, idx) => {
-                return <div key={idx}>{item}</div>
-            })}
-        </div>
-    </div >
+	return <div className={clsx("relative overflow-hidden", props.className)} style={props.style}>
+		<div className="absolute flex overflow-hidden transition-all duration-500" ref={el => { sliderRef = el }}>
+			{props.children.map((item, idx) => {
+				return <div key={idx}>{item}</div>
+			})}
+		</div>
+	</div >
 }
--- app/impl/window_slide_impl.tsx
+++ app/impl/window_slide_impl.tsx
@@ -1,11 +1,26 @@
+"use client"
+import React from "react";
 import WindowSlide from "../comps/window_slide";
 
+const INTERVAL_MS = 2000;
+
 export default function WindowSlideImpl() {
-    // return <WindowSlide intervalMs={3000} className="border-white border-2" style={{width: 700, height: 500}}>
-    return <WindowSlide intervalMs={3000} className="border-white border-2 w-[700px] h-[500px]">
-        <div className="bg-[rgb(255,0,0)] w-[500px] h-[500px]">A</div>
-        <div className="bg-[rgb(0,255,0)] w-[500px] h-[500px]">B</div>
-        <div className="bg-[rgb(0,0,255)] w-[500px] h-[500px]">C</div>
-        <div className="bg-[rgb(100,100,100)] w-[500px] h-[500px]">D</div>
-    </WindowSlide>
+	const [pageIdx, setPageIdx] = React.useState(0);
+
+	React.useEffect(() => {
+		let _pageIdx = 0;
+		const intervalId = setInterval(() => {
+			_pageIdx = (_pageIdx + 1) % 4;
+			setPageIdx(_pageIdx);
+		}, INTERVAL_MS);
+
+		return () => clearInterval(intervalId);
+	}, []);
+
+	return <WindowSlide eleIdx={pageIdx} className="border-white border-2 w-[700px] h-[500px]">
+		<div className="bg-[rgb(255,0,0)] w-[500px] h-[500px]">A</div>
+		<div className="bg-[rgb(0,255,0)] w-[500px] h-[500px]">B</div>
+		<div className="bg-[rgb(0,0,255)] w-[500px] h-[500px]">C</div>
+		<div className="bg-[rgb(100,100,100)] w-[500px] h-[500px]">D</div>
+	</WindowSlide>
 }
